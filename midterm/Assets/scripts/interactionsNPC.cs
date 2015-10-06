67b61a94f881d725538452591409f9ed7e4ae18f
bas511 <bas511@nyu.edu>
2015-10-06 12:03:56 -0400

    midterm initial commit

---
+++ midterm/Assets/scripts/interactionsNPC.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class interactionsNPC : MonoBehaviour {
+
+	public Text prompt;
+	public Text dialogue;
+	public Transform teacher1;
+	public Transform teacher2;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Vector3.Distance(transform.position, teacher1.transform.position) < 1) {
+			Debug.Log ("TEACHER1");
+			prompt.text = "Press 'Space' to talk.";
+			if (Input.GetKeyDown(KeyCode.Space)) {
+				dialogue.text = " TEACHER 1 TEXT";
+			}
+		} else if (Vector3.Distance(transform.position, teacher1.transform.position) > 2) {
+			//prompt.text = "";
+			dialogue.text = "";
+		}
+		if (Vector3.Distance(transform.position, teacher2.transform.position) < 1) {
+			Debug.Log ("TEACHER2");
+			prompt.text = "Press 'Space' to talk.";
+			if (Input.GetKeyDown(KeyCode.Space)) {
+				dialogue.text = " TEACHER 2 TEXT";
+			}
+		} else if (Vector3.Distance(transform.position, teacher2.transform.position) > 2) {
+			//prompt.text = "";
+			dialogue.text = "";
+		}
+
+
+	}
+}
---
+++ midterm/Assets/scripts/movePlayer.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movePlayer : MonoBehaviour {
+
+	bool firstPerson = false;
+	public bool zoomable = false; // possibility of a glasses-like item enabling zoom
+	//bool zoomed = false;
+	Rigidbody rbody;
+	Vector3 lastAngles, lastCamPos;
+	Vector3 inputVector;
+	// Use this for initialization
+	void Start () {
+		rbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.F)) {
+			if (!firstPerson) {
+				lastAngles = Camera.main.transform.eulerAngles;
+				lastCamPos = Camera.main.transform.position;
+				Camera.main.transform.localPosition = new Vector3(0f, 0.3f, 0f);
+				Camera.main.transform.eulerAngles = new Vector3(0f, lastAngles.y, 0f);
+			}
+			else {
+				Camera.main.transform.position = lastCamPos;
+				Camera.main.transform.eulerAngles = lastAngles;
+			}
+			firstPerson = !firstPerson;
+			Debug.Log ("SWITCH");
+			Debug.Log (firstPerson.ToString());
+		}
+		Debug.Log(zoomable);		
+		if (firstPerson) {
+
+			//Camera.main.transform.position = transform.position;
+			Camera.main.transform.eulerAngles += new Vector3(-Input.GetAxis("CamVertical"), 
+			                                                 Input.GetAxis("CamHorizontal"), 0f);
+
+			//if (Input.GetAxis("CamZoom")) {
+				Camera.main.transform.localPosition = Camera.main.transform.TransformDirection(0f, 0f, Input.GetAxis("CamZoom"));
+			//}
+
+			/*
+			if (zoomable) {
+				Camera.main.transform.position += Camera.main.transform.TransformDirection(0f, 0f, Input.GetAxis("CamZoom") * Time.deltaTime);
+				if (Camera.main.transform.localPosition.z < 0) {
+					Debug.Log ("STOP.");
+					Debug.Log (Camera.main.transform.localPosition.z);
+					Camera.main.transform.localPosition = new Vector3(0f, 0.3f, 0f);
+				}
+
+				Debug.Log (Camera.main.transform.position);
+			}
+			*/
+		}
+		else {
+			inputVector = new Vector3( 0f, 0f, Input.GetAxis ("Vertical") );
+			transform.eulerAngles += new Vector3(0f, Input.GetAxis("Horizontal") * 60f * Time.deltaTime,0f);
+		}
+		//Debug.Log(rbody.velocity);
+		// Camera.main.transform.eulerAngles = new Vector3(0f, -transform.eulerAngles.y, 0f);
+		//Camera.main.transform.position = 
+	}
+
+	void FixedUpdate () {
+		rbody.velocity = (firstPerson) ? Vector3.zero : transform.TransformDirection(inputVector * 5f);
+	}
+}
