1dfa2ec9aa2b96048cdc07919ba75e788a186b55
bas511 <bas511@nyu.edu>
2015-10-20 14:32:46 -0400

    added title screen

---
+++ midterm/Assets/scripts/startGame1.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class startGame1 : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.S)){
+			Application.LoadLevel (1);
+		}
+	}
+}
